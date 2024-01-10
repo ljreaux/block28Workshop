efbead25802cf8e662bbce2d1d2a2aea85981ac8
Larry Reaux <larryreaux@gmail.com>
2024-01-09 19:28:12 -0600

    finished "Router Configuration" tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div id="container">
-      <h1>Hello React Router!</h1>
       <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<h1>Blue</h1>} />
+          <Route path="/red" element={<h1>Red</h1>} />
+        </Routes>
+      </div>
     </div>
   );
 }
